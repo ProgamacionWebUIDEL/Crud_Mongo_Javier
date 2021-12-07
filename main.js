10a403adb506611ede44f83c0ff304a24d360189
jA71 <isjavier77@gmail.com>
2021-12-07 16:03:55 -0500

    Se actualiza el archivo con la tarea enviada

--- main.js
+++ main.js
@@ -13,6 +13,37 @@ app.get("/Suma",(req,res)=>{
     res.json({mensaje:"Resultado de la Suma: "+suma});
 });
 
+app.get("/Resta",(req,res)=>{
+    var datos=(req.query);
+    var resta=Number(datos.n1)-Number(datos.n2);
+    res.json({mensaje:"Resultado de la Resta: "+resta});
+});
+
+app.get("/Producto",(req,res)=>{
+    var datos=(req.query);
+    var produc=Number(datos.n1)*Number(datos.n2);
+    res.json({mensaje:"Resultado del Producto: "+produc});
+});
+
+app.get("/Division",(req,res)=>{
+    var datos=(req.query);
+    var div=Number(datos.n1)/Number(datos.n2);
+    res.json({mensaje:"Resultado de la Division: "+div});
+});
+
+app.get("/Calcular_Edad",(req,res)=>{
+    var hoy = new Date();
+    var cumpleaños = new Date(fecha);
+    var edad = hoy.getFullYear()-cumpleaños.getFullYear();
+    var m = hoy.getMonth()-cumpleaños.getMonth();
+
+    if (m<0|| (m===0 && hoy.getDate() < cumpleaños.getDate())){
+        edad--;
+    }
+    return edad;
+});
+
+
 app.listen(3000,()=>{
     console.log("Servidor Iniciado")
 });
